99684357b1dd901f502412e4846981ff0e198613
raselsiu <raselqax@gmail.com>
2023-12-14 00:27:21 +0600

    template ready

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', [PageController::class, 'index']);
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
